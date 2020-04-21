b1e7d790b911ea7bc04f78440f68f2e291af3cd3
varshadevops <56285522+varshadevops@users.noreply.github.com>
2020-04-21 17:37:58 +0800

    Add files via upload

---
+++ FirstSonarJava.java
@@ -0,0 +1,23 @@
+package com.varsha.sonarqube;
+import java.io.File;
+import java.io.IOException;
+
+public class FirstSonarJava {
+	
+	public static final String abc="";
+	public static String [] strings1 = {"first","second"};
+	
+	
+	public static void main(String[] args) {
+		String MayBeNull = null;
+		System.out.println("Hello SonarQube "+MayBeNull);
+		 
+		for (int i = 10; i < 10; i++) { 
+			System.out.println("Loop not true even once");
+		}
+			
+	}
+
+}	
+	
+
